f3091a76bfdf7842b68d603aebe6669a1d5661fa
Shubham Gupta <gshubham934@gmail.com>
2021-03-11 19:52:52 +0530

    FINAL

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.slider').slick({
+
+      arrow: false,
+      dots: true,
+      appendDots:'.slider-dots',
+      dotsClass: 'slick-dots'
+      
+    });
+  });
+
+  let hamberger = document.querySelector('.hamberger');
+  let times = document.querySelector('.times');
+  let mobileNav = document.querySelector('.mobile-nav');
+
+  hamberger.addEventListener('click',function(){
+
+    mobileNav.classList.add('open');
+  });
+
+
+  times.addEventListener('click',function(){
+
+    mobileNav.classList.remove('open');
+  });
